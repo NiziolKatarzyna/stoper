b52687e808383e58950ccf39c16cb314f4df3dfc
Katarzyna Nizioł <niziol.katarzyna25@gmail.com>
2023-11-23 22:41:06 +0100

    init project, add Stoper component, add format time, add start

---
+++ src/Component/Stoper/Stoper.js
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import styles from './Stoper.module.scss';
+import { useEffect } from 'react';
+import { format } from 'date-fns';
+
+const Stoper = ({ milliseconds }) => {
+  const formattedTime = format(new Date(milliseconds), 'HH:mm:ss.SSS');
+  const customFormattedTime = formattedTime.substring(0, 12);
+
+  return (
+    <section>
+      <div className={styles.stoper}>
+        <p>{customFormattedTime}</p>
+      </div>
+    </section>
+  );
+};
+export default Stoper;
